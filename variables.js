b04c2dc44210dd21ce683edc2ed19e06a8d5ee12
Bisra Batool <bisrabatool345@gmail.com>
2023-12-30 07:05:18 -0800

    first topic

---
+++ variables.js
@@ -0,0 +1,37 @@
+// key words in javascript that used to declare varibale
+// 1 var 
+// 2 let
+// 3 const
+// var x = 10; // global-scope or function-scope variable
+// var z = 20;
+// let b = 40; //block-scoped variable
+// const d = "hello"; // declare a variable that cannot reassigned  also block-scoped
+// var
+var a = "hi"
+var b = "i";
+var c = "am";
+var d = b + c;
+ 
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+// let
+let x = "hello"
+let y = "bisra";
+let z = "batool";
+let w = x +" " +y +" "+ z;
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(w);
+
+// const
+const name = "bisra"
+const age = "100";
+const gender = "female";
+
+console.log(name);
+console.log(age);
+console.log(gender);
