7954869f8b49d9943724fa8f9fe56e4c520f5562
Bisra Batool <bisrabatool345@gmail.com>
2023-12-30 07:31:31 -0800

    new topic

---
+++ arrays.js
@@ -0,0 +1,150 @@
+// basic method for array
+// *push: is used for insert the value in array last 
+// *pop: is used for delete the value in array first
+// *unshift: is used for insert the value in array first
+// *shift:delete the first value
+// *splice:insert or elete the value in array from everywhere
+// *slice:copy the value of array
+// var arr = [1,2,3,4,5,6]
+// arr.push(9)    
+// document.write(arr);  
+// arr.pop()
+// document.write(arr)
+// arr.unshift(0)
+// document.write(arr)
+// arr.shift()
+// document.write(arr)
+// arr.splice(0,0)
+// document.write(arr)
+// arr.slice()
+// document.write(arr)
+
+
+// // user response
+// var flower =["rose","jasmin","bella"]
+// alert(flower(0));
+// alert(flower(1));
+// alert(flower(2));
+// console.log(flower);
+
+// // mixed Array
+// var mixedarray = ["rose","1","pink" ]
+
+// //
+// var cites =['karachi','islambad','multan'];
+// alert(cites[1]);//acesse ,read value
+// //modify value
+// cites[2] ="faislabad";
+// console.log(cites)
+
+//pop and push
+// var LhoppingList =['milk ','bread','apple'];
+// console.log(ShoppingList.length);
+// console.log(ShoppingList)
+// ShoppingList[1] = 'banana';//update modify value
+// //for last element
+// var LastElement = ShoppingList[ShoppingList.lenght-1];
+// var SomeOtherElement = ShoppingList[ShoppingList.lenght-2];
+// var SomeElement = ShoppingList[1+1]
+
+// var Tea = ['water','milk'];
+// //splice is use for add and remove array element
+// Tea.splice [1, 0 , 'tea','sugar'];
+// console.log(Tea);
+// var Maggi =['water','maggi','masala'];
+// console.log('');
+// Maggi.splice(2, 0, 'vegetales');
+// console.log("");
+// Maggi.shift()
+// console.log("");
+// 
+// var Count = [1,2,3,6,7,8];
+// Count.splice[3,0,'4',4,0,'5'];
+// console.log(Count);
+// 
+// let array = [1,2,3,4,5,6,7,8,9,10]
+// for (let i=0; i<= array.lenght-1; i++){
+    // array[i] = array[i]+1
+// }
+// console.log(array)
+// 
+// var cites=["Ny","La","Cg","Isb"];
+// for(let i=0; i<= cites.lenght-1; i++){
+//     cites[i] = cites[i]+ "City";
+// }
+// console.log(cites);
+// 
+// var table= +prompt("enter your table namuber :",2);
+// var tablelenght= +prompt("enter your table lenght:",10);
+// for(let i=1; i<= tablelenght; i++){
+    // var element = table*i ;
+    // console.log('$(table) * $(i) = $(element)')
+//    console.log(table + "*" + i+ "=", table*i)
+
+// }
+// for odd number
+// for(let i=1; i<= 100; i = i+2){
+//     document.write(i)
+// }
+// for even num
+// for(let i=2; i<=100; i = i+2){
+//     console.log(i)
+// }
+
+//loop with push method
+// let num =[];
+// for (let i= 1; i <= 10 ;  i++){
+//     num.push(i)
+
+// }
+// console.log(num);
+
+// for an array lenght
+// const array=[1,3,'a',6,7];
+// let total = 0;
+// for (let i = 0; i < array.length; i++) {
+//     total+=1; 
+// }
+// console.log(total);
+
+// how many times a appears
+// const checkvar =[1,3,'a',6,'a',7,'a']
+// let index = 0;
+// for (let i = 0; i < checkvar.length; i++) {
+//     const element = checkvar[i];
+//     if (element ==="a"){
+//         index+=1;
+//     }
+// }
+// console.log (index);
+// //
+//  const checkelement=[1,2,3,'a',6,7]
+// let index2 = 0;
+// for (let i = 0; i < checkelement.length; i++) {
+//     const element = checkelement[i];
+//     if (checkelement > 0)console.log("the given array has number");
+//     else console.log ("it has no number");
+
+ 
+// }
+// const countnumber=[1,2,3,4]
+// let index3 = 0;
+// for (let i = 0; i < countnumber.length; i++) {
+//     const element = countnumber[i];
+//     if (element > 0){
+//         index3+=;
+//     }
+    
+// }
+// console.log (index);
+//
+
+
+
+
+
+
+
+
+
+
