c32aa8fcc5bf4c793c46ce4eb08903d19cb35eda
Bisra Batool <bisrabatool345@gmail.com>
2024-08-05 08:12:58 -0700

    some example

---
+++ datatype.js
@@ -0,0 +1,33 @@
+// what is data????
+// information or user defined
+// Data types.
+// integars 1,2,......, -1, -2
+// float 1.1, 0.4, 0.555, 122.3
+// string "" abcd, words, sentenced, char
+// boolean only 2 values true and false
+// undefined
+// null
+
+
+//here some simple example of how to check diffrent data type
+var a = "10";
+typeof(a);
+console.log(typeof(a));//string
+// 
+var b = 304;
+typeof(b)
+console.log(typeof(b)); //integar 
+// 
+var c = 23.4;
+typeof(c);
+console.log(typeof(c)); //folat
+// 
+var d = true;
+var e = false;
+console.log(typeof(d,e)) //true and false data is bolean data
+// 
+var f = 
+console.log(typeof(f)); //undefine because vale is not define
+// 
+var g = null;
+console.log(typeof(g));//object or null
